36dab485427d58b4f5fb95ee8f6dbdc8f7e60516
Youssef Ahmed <yousseffcai@gmail.com>
2025-03-11 22:48:16 +0200

    Bellman

---
+++ bellman.cpp
@@ -0,0 +1,20 @@
+// #define pii pair<int, int>
+vector<pair<int, pii>> adj;
+int n, m;
+// #define ll long long
+ll mn = oo; // oo = 0x3f3f3f3f
+ll dis[N];
+
+int bellman(){
+    for (int i = 0; i < n; i++) {
+        for (auto j: adj){
+            int from = j.F, to = j.S.F, w = j.S.S;
+            if (dis[to] > dis[from] + w) {
+                dis[to] = dis[from] + w;
+                mn = min(mn, dis[to]);
+                if (i == n - 1) return 1;
+            }
+        }
+    }
+    return 0;
+}
