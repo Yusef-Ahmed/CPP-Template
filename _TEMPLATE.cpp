681164b6db0f1ab63f162b19832f736d55d7e195
Youssef Ahmed <yousseffcai@gmail.com>
2025-03-11 22:56:05 +0200

    Main template

---
+++ _TEMPLATE.cpp
@@ -0,0 +1,61 @@
+﻿/*
+ ████████╗░█████╗░███╗░░██╗░░░░░██╗██╗██████╗░░█████╗░██╗░░░██╗
+ ╚══██╔══╝██╔══██╗████╗░██║░░░░░██║██║██╔══██╗██╔══██╗██║░░░██║
+ ░░░██║░░░███████║██╔██╗██║░░░░░██║██║██████╔╝██║░░██║██║░░░██║
+ ░░░██║░░░██╔══██║██║╚████║██╗░░██║██║██╔══██╗██║░░██║██║░░░██║
+ ░░░██║░░░██║░░██║██║░╚███║╚█████╔╝██║██║░░██║╚█████╔╝╚██████╔╝
+ ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░
+*/
+#include <bits/stdc++.h>
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define all(v)  v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pll pair<ll, ll>
+using namespace std;
+void TANJIR0U();
+const int mod = 998244353, oo = 0x3f3f3f3f, N = 1e5 + 9,
+          LOG = 18, P1 = 31, P2 = 37;
+const ll loo = 0x3f3f3f3f3f3f3f3f;
+
+/*
+ *   0% Talent
+ * 100% Hard Work
+ */
+
+void solve() {
+    
+}
+
+int main() {
+    TANJIR0U();
+//    clock_t z = clock();
+//    pre();
+    int t = 1;
+//    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+//    cout << "RUN TIME IS : " << ((double) (clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
+
+void TANJIR0U() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+#endif
+}
