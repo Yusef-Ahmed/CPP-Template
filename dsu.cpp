e73805d989b02d874c6cf3750c24148c49a75a39
Youssef Ahmed <yousseffcai@gmail.com>
2025-03-26 13:59:35 +0200

    Feature: dsu

---
+++ dsu.cpp
@@ -0,0 +1,43 @@
+// const int N = 1e5 + 9;
+
+struct DSU {
+  int parent[N], groupsize[N];
+
+  DSU() {
+      for (int i = 0; i < N; i++) {
+          parent[i] = i;
+          groupsize[i] = 1;
+      }
+  }
+
+  int find_leader(int i) {
+      if (parent[i] == i) return i;
+
+      return parent[i] = find_leader(parent[i]);
+  }
+
+  bool same_group(int x, int y) {
+      int leader1 = find_leader(x),
+          leader2 = find_leader(y);
+      return leader1 == leader2;
+  }
+
+  void unite(int x, int y) {
+      int leader1 = find_leader(x),
+          leader2 = find_leader(y);
+
+      if (leader1 == leader2) return;
+
+      if (groupsize[leader2] > groupsize[leader1]) {
+          swap(leader1, leader2);
+      }
+
+      groupsize[leader1] += groupsize[leader2];
+      parent[leader2] = leader1;
+  }
+
+  int get_size(int x) {
+      int leader = find_leader(x);
+      return groupsize[leader];
+  }
+};
