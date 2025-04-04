780a12f2b05f1fe6f0b4438fc579e25c4a3a476d
Youssef Ahmed <yousseffcai@gmail.com>
2025-04-04 19:16:34 +0200

    extended GCD

---
+++ extendedGCD.cpp
@@ -0,0 +1,18 @@
+void movestep(int& a, int& b, int q) {
+    int next = a - q * b;
+    a = b;
+    b = next;
+}
+
+int extendedGCD(int a, int b, int& x0, int& y0) {
+    int r0 = a, r1 = b, x1, y1;
+    x1 = y0 = 0;
+    x0 = y1 = 1;
+    while (r1 != 0) {
+        int q = r0 / r1;
+        movestep(x0, x1, q);
+        movestep(y0, y1, q);
+        movestep(r0, r1, q);
+    }
+    return r0;
+}
